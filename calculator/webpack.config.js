eaaf71e9aec97e3b2b8a48c65ef517612939bf81
Michael Jonathan Lee <code.mjlee@gmail.com>
2018-10-19 14:22:09 +0200

    Updated babel for calculator.

--- calculator/webpack.config.js
+++ calculator/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: './src/index.jsx',
